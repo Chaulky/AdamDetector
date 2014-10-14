187e49ff83b82a92ac407e71075ba2371c4d6834
Greg Ayer <chaulky@gmail.com>
2014-10-13 19:14:49 -0700

    Got things working with averaged sensor readings and 10 second interval

--- adam_detector/adam_detector.ino
+++ adam_detector/adam_detector.ino
@@ -4,21 +4,54 @@ Turns on an LED when and object (Adam) is within distance threshold.
 */
 
 const int IR_READ = 1;
-const int IR_PIN = 2;
-const int LED = 0;
+const int LED_HERE = 0;
+const int LED_GONE = 1;
+const int NUM_READINGS = 10;
+const float VOLTS_PER_UNIT = 5.0 / 1024;
+const float THRESHOLD = 0.5; // Voltage threshold from sensor to indicate Adam is here.
+
+// Use average of multiple readings to smooth sensor input.
+int sensorReadings[NUM_READINGS];
+
+// Use millis() to limit the frequency of Adam checks
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;
+unsigned long adamCheckInterval = 10000;
 
 void setup() {
-  pinMode(LED, OUTPUT);
+  pinMode(LED_HERE, OUTPUT);
+  pinMode(LED_GONE, OUTPUT);
 }
 
 void loop() {
-  delay(500);
-  int sensorValue = analogRead(IR_READ);
-  float voltage = sensorValue * (5.0 / 1023.0);
-  if (voltage >= 0.5) {
-    digitalWrite(LED, HIGH);
+  currentMillis = millis();
+  
+  if (previousMillis == 0 || currentMillis - previousMillis > adamCheckInterval) {
+    previousMillis = currentMillis;
+
+    if (isAdamHere()) {
+      digitalWrite(LED_HERE, HIGH);
+      digitalWrite(LED_GONE, LOW);
+    }
+    else {
+      digitalWrite(LED_HERE, LOW);
+      digitalWrite(LED_GONE, HIGH);
+    }
   }
-  else {
-    digitalWrite(LED, LOW);
+}
+
+boolean isAdamHere() {
+  int total = 0;
+  float average = 0;
+  for (int index = 0; index < NUM_READINGS; index++) {
+    total = total + analogRead(IR_READ);
+
+    delay(20);
   }
+
+  // calculate the average:
+  average = (total * 1.0) / NUM_READINGS;
+  
+  float voltage = average * (5.0 / 1024);
+  return voltage >= THRESHOLD;
 }
